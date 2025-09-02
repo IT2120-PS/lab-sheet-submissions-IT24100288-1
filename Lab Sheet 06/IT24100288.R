196262ffee01b8e9c83af741ac1f4536211dba4d
IT24100288 <it24100288@my.sliit.lk>
2025-09-02 14:28:07 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24100288.R
@@ -0,0 +1,18 @@
+#1
+setwd("C:\\Users\\it24100288\\Desktop\\IT24100288\\Lab06")
+#i
+#Binomial Distribution
+
+#ii
+1 - pbinom(46, 50, 0.85, lower.tail = TRUE)
+pbinom(46, 50, 0.85, lower.tail = FALSE)
+
+#2
+#i
+# Number of customer calls received per hour
+
+#ii
+# Poisson distribution
+
+#iii
+dpois(15, 12)
